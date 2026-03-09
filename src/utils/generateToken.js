97432caa66d752ae1fdd72e3b86a023247a48e34
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2026-03-09 10:16:22 +0530

    Update generateToken.js

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,8 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-
-  
   if (!process.env.JWT_SECRET) {
     console.error("ERROR: JWT_SECRET is missing from .env");
     return null;
@@ -14,12 +12,10 @@ export const generateToken = (userId, res) => {
       expiresIn: process.env.JWT_EXPIRES_IN || "7d",
     });
 
-
-
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
