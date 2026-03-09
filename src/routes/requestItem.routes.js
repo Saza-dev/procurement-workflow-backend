ee2c53afa253f7d8b7988ad1c14a4a6c44a78714
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2026-03-09 09:52:52 +0530

    Update requestItem.routes.js

--- src/routes/requestItem.routes.js
+++ src/routes/requestItem.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMiddleware } from "../../../../procurement/backend/src/middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 import {
   addInvoice,
   addItem,
